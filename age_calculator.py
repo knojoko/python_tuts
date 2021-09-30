533a2621a357013934d4b64660d75867a653fe8b
Khayalethu Prudent Nojoko <knojoko@gmail.com>
2021-09-30 19:41:50 +0200

    update2

--- age_calculator.py
+++ age_calculator.py
@@ -2,24 +2,23 @@ import datetime
 
 year = datetime.datetime.now().year
 
+
 # name = input("Enter your first name:  ")
 # year_of_birth = int(input("Enter your year of birth:  "))
 # age = year - year_of_birth
 
-def name(y):
-    y = input("Enter your first name: ")
-    print(y)
 
-def age_calculator(x):
-    age = year - x
-    return age
-
-name = name("khaya")
-age = age_calculator(int(input("Enter birth year:  ")))
+def name():
+    x = input("Enter your first name: ")
+    return x
 
 
+def age_calculation(x):
+    years = year - x
+    return years
 
-print(f"Hi {name} you are {age} years of age")
 
+name = name()
+age = age_calculation(int(input("Enter birth year:  ")))
 
-
+print(f'Hi {name} you are {age} years of age')
