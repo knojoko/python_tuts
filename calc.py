cfa84fb3ec8bd47a9d130ebaa30b76224d1fe8c0
Khayalethu Prudent Nojoko <knojoko@gmail.com>
2021-10-01 12:40:52 +0200

    Ansible - Ubuntu update play
    This play will update all ubuntu distro packaged and update security patches.

---
+++ calc.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x + y
+
+
+def multiply(x,y):
+    return x * y
+
+
+def divide(x,y):
+    return x / y
+
+
+def subtract(x,y):
+    return x - y
+
+add = add(2,5)
+divide = divide(2,5)
+subtract = subtract(2,5)
+multiply = multiply(2,5)
+
+print(add, subtract, multiply, divide)
