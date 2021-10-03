27e8db3f45f39e4ef54c2bb71b7e6aa3c132c967
Khayalethu Prudent Nojoko <knojoko@gmail.com>
2021-10-03 16:07:47 +0200

    updated class syntax for name

--- age_calculator.py
+++ age_calculator.py
@@ -6,15 +6,15 @@ year = datetime.datetime.now().year
 # year_of_birth = int(input("Enter your year of birth:  "))
 # age = year - year_of_birth
 
-def name(y):
-    y = input("Enter your first name: ")
-    print(y)
+def name():
+    name = input("Enter your first name: ")
+    return name
 
 def age_calculator(x):
     age = year - x
     return age
 
-name = name("khaya")
+name = name()
 age = age_calculator(int(input("Enter birth year:  ")))
 
 
